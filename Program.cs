5ef1f1092e0a17af33eeb7a70f9e20f693d67b47
Soshi <soshi.mimicry@gmail.com>
2020-04-03 16:15:10 +0200

    Zweite Methode erklaert i nicht...

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AB6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*public static double Pow(double x double y);*/
+            double i = 1;
+            double kubikWert;
+            
+            do
+            {
+                kubikWert = Math.Pow(i, 3);
+                i = i + 1;
+            } while (kubikWert < 5000);
+
+            Console.WriteLine("Erste Methode:\ni: {0}, Kubikwert: {1}", i, kubikWert);
+
+
+            /*Hier ist i nicht aus der Schleife verfuegbar...*/
+            for (i = 1; i > 500; i = i + 1)
+            {
+                kubikWert = Math.Pow(i, 3);
+            } while (kubikWert < 5000) ;
+            Console.WriteLine("Zweite Methode:\ni: {0}, Kubikwert: {1}", i, kubikWert);
+
+        }
+    }
+    
+}
